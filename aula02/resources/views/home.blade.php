2ec30ce847e160c2338d28c33ead1d037eb2fe11
Victor Rodrigues <victorrodrigue288@gmail.com>
2025-02-21 22:13:52 -0300

    Revisão aula01

---
+++ aula02/resources/views/home.blade.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+</head>
+<body>
+    <h1>Primeira aula do João</h1>
+    <h2>Pokémon é ruim</h2>
+    <h3>Piores pokémons</h3>
+    <ol>
+        <li>Charmander</li>
+        <li>Bulbasaur</li>
+    </ol>
+
+</body>
+
+</html>
