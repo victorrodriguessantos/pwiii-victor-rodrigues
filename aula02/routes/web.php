2b9b9982cdbb775bab1b0ff749e7548130cea519
Victor Rodrigues <victorrodrigue288@gmail.com>
2025-02-24 22:30:11 -0300

    Aula seeds e db

--- aula02/routes/web.php
+++ aula02/routes/web.php
@@ -5,6 +5,26 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
 Route::get('/home', function () {
     return view('home');
+});
+
+Route::get('/users/{params}/{params2}', function ($params, $params2) {
+    return $params . ' ' . $params2;
+});
+
+Route::prefix('usuarios')->group(function() {
+    Route::get('/edit', function(){
+        return 'edit';
+    });
+
+    Route::get('/delete', function(){
+            return 'delete';
+    });
+
+    });
+
+Route::get('/dados-de-usuarios/{user}', function(\App\Models\User $user){
+    return $user;
 });
